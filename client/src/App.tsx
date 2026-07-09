3e82f4f866cb79c879acd0b7d2f6a6a16f7cec48
unknown <sersid782@gmail.com>
2026-07-09 15:48:35 +0700

    Fix: BrowserRouter + _redirects for Cloudflare Pages routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Watch } from "./pages/Watch";
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/watch/:roomId" element={<Watch />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
